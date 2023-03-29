9a58aeb6f29d6e027487f133cca74eaaae9e4700
Reza Ghanbari <reza001391@gmail.com>
2023-03-29 16:19:41 +0430

    show filters in DOM (dynamic) and edit db.json

--- assest/js/app.js
+++ assest/js/app.js
@@ -4,7 +4,7 @@ const filters = {
   searchItems: "",
 };
 const searchInput = document.querySelector("#search");
-
+const productCenter = document.querySelector(".products-center");
 // DOM CONTENT LOADED ----------------------------------------------------
 document.addEventListener("DOMContentLoaded", () => {
   axios.get("http://localhost:3000/items").then((res) => {
@@ -21,6 +21,25 @@ function renderProducts(_product, _filters) {
     return p.title?.toLowerCase().includes(_filters.searchItems?.toLowerCase());
   });
   console.log(filterdProducts);
+  //
+  productCenter.innerHTML = "";
+  filterdProducts.forEach((el, index) => {
+    // create
+    const div = document.createElement("div");
+    div.classList.add("product");
+    // content
+    div.innerHTML = `
+    <div class="img-container">
+            <img class="product-img" src="assest/${el.image}" "alt="p-${index}" />
+          </div>
+          <div class="product-desc">
+            <p class="product-price">${el.price} $</p>
+            <p class="product-title">${el.title}</p>
+          </div>
+    `;
+    // append
+    productCenter.appendChild(div);
+  });
 }
 
 // EVENTs ----------------------------------------------------------------------
